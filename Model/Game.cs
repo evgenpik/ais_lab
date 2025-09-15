aae1669f67125eee461cbfb4cbf2be108a09c610
Maxonchik <maxkucherov06@yandex.ru>
2025-09-15 08:21:37 +0700

    Добавил класс Game и перечесление Genre

---
+++ Model/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Game
+    {
+        public Guid Id { get; }
+        public string Title { get; set; }
+        public Genre GameGenre { get; set; }
+        public string Developer { get; set; }
+        public int ReleaseYear { get; set; }
+
+        public string[] Platforms { get; set; }
+
+        public int Rating { get; set; }
+
+
+    }
+}
