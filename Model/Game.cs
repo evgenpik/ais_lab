2858793caeca40261cee9d7dd142178b75d9b683
evgenpik <evgenpik06@gmail.com>
2025-10-08 20:19:48 +0700

    Добавила интерфейс IDomainObject, слой DaraAccessLayer и подгрузила пакеты Nuget (EF и Dapper)

--- Model/Game.cs
+++ Model/Game.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    public class Game
+    public class Game: IDomainObject
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
---
+++ aislab_1/DataAccessLayer/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Delete(Guid id);
+        IEnumerable<T> ReadAll();
+        T ReadById(Guid id);
+        void Update(T entity);
+    }
+
+}
