90cf7c76d03554b0910c00794833119c7c5c46ad
Maxonchik <maxkucherov06@yandex.ru>
2025-10-30 01:16:32 +0700

    Feat: Добавил сущность Platform, поменял св-во в Game

--- Model/Game.cs
+++ Model/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,21 @@ namespace Model
         public string Developer { get; set; }
         public int ReleaseYear { get; set; }
 
-        public string Platform { get; set; }
-
+        //public string Platform { get; set; }
         public int Rating { get; set; }
+
+        // 2. Добавил свойство для хранения внешнего ключа (ID платформы).
+        //    Тип Guid, чтобы соответствовать новой таблице Platforms.
+        public Guid PlatformId { get; set; }
+
+        // 3. Добавил "ненастоящее" свойство для удобного отображения.
+        //    Атрибут [NotMapped] говорит Entity Framework, чтобы он НЕ пытался
+        //    создать такую колонку в базе данных. Dapper его просто проигнорирует.
+        //    Это свойство будет заполняться только при чтении с помощью SQL JOIN.
+        [NotMapped]
+        public string PlatformName { get; set; }
+
+        
 
 
     }
